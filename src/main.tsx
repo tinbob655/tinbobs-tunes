a33740d3a892af8f0e5ce986623eeb83908156e5
Will Rutland <wrutland07@gmail.com>
2025-07-15 15:33:10 +0100

    created separate file for forms css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './scss/index.scss';
 import './scss/text.scss';
+import './scss/forms.scss';
 import AppRoutes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/multiPageComponents/header';
