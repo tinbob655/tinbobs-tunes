02601602028858cf1cb02a2c3c6a5fc37950c9d0
Will Rutland <wrutland07@gmail.com>
2025-07-16 21:14:10 +0100

    created a component which gives a dropdown of all tracks

---
+++ src/components/multiPageComponents/allTracksDropDownList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import albumsData from '../albums/albumsData.json' assert {type: 'json'};
+import singlesData from '../singles/singlesData.json' assert {type: 'json'};
+
+export default function AllTracksDropDownList():React.ReactElement {
+
+    function getOptions():React.ReactElement[] {
+        let tempOptions:React.ReactElement[] = [];
+        let allTracks:string[] = [];
+        
+        //add all singles to the array
+        singlesData.forEach((single) => {
+            allTracks.push(single.frontendName);
+        });
+
+        //add all tracks in albums to the array
+        Object.entries(albumsData).forEach((album) => {
+            album[1].tracks.forEach((albumTrack) => {
+                allTracks.push(albumTrack[1]);
+            });
+        });
+
+        //sort the tracks in alphabetical order
+        allTracks.sort();
+
+        //now generate an html option for each track
+        allTracks.forEach((track) => {
+            tempOptions.push(
+                <option value={track}>
+                    {track}
+                </option>
+            );
+        });
+
+        return tempOptions;
+    };
+
+    return (
+        <React.Fragment>
+            {getOptions()}
+        </React.Fragment>
+    );
+};
