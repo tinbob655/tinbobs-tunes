8afbe2e232d074640dfd7c125b5320999e5f29ee
Will Rutland <wrutland07@gmail.com>
2025-07-20 15:16:15 +0100

    added function to get track audio files

--- src/components/pages/playlists/playlistClass.ts
+++ src/components/pages/playlists/playlistClass.ts
@@ -1,3 +1,6 @@
+import albumsData from '../albums/albumsData.json' assert {type: 'json'};
+import singlesData from '../singles/singlesData.json' assert {type: 'json'};
+
 export class playlistClass {
 
     #name:string;
@@ -59,5 +62,46 @@ export class playlistClass {
         });
 
         return res;
+    };
+
+    getTrackAudioFiles():string[][] {
+        let tempAudioFiles:string[][] = [];
+        this.#tracks.forEach((track) => {
+            let trackFound:boolean = false;
+
+            //see if the track is a single
+            singlesData.forEach((single) => {
+                if (single.frontendName === track) {
+
+                    //the track was a single
+                    trackFound = true;
+                    tempAudioFiles.push(
+                        [single.fileName, 'single']
+                    );
+                };
+            });
+
+            //if it was not a single, search all albums for the track
+            if (!trackFound) {
+                Object.entries(albumsData).forEach((album) => {
+                    let albumName:string = album[0];
+                    album[1].tracks.forEach((albumTrack) => {
+                        if (albumTrack[1] === track) {
+
+                            //the track was found in an album
+                            trackFound = true;
+                            tempAudioFiles.push(
+                                [albumTrack[0], albumName]
+                            );
+                        };
+                    });
+                });
+            };
+
+            //if the track still wasn't found, throw an error
+            if (!trackFound) throw new Error(`Could not find track called ${track}`);
+        });
+
+        return tempAudioFiles;
     };
 };
