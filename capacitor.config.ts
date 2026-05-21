fd800afa244dcb042a58b9b98795ed23eb5a0cf7
Will Rutland <wrutland07@gmail.com>
2026-05-21 16:05:00 +0100

    reset ready for new branch

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.newrinaldi.site',
-  appName: "Tinbob's Tunes",
-  webDir: 'dist'
-};
-
-export default config;
