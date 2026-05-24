51875d1be41091c0961146c2a8825978e892cf3e
Will Rutland <wrutland07@gmail.com>
2026-05-24 12:24:10 +0100

    created footer

--- src/components/multiPage/footer/Footer.tsx
+++ src/components/multiPage/footer/Footer.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import logo from '../../../assets/images/NR logo.png';
+import './footer.scss';
 
 export default function Footer():React.ReactElement {
 
     return (
         <footer>
+            <div className={"sectionDivider"}/>
+            <p>
+                App brought to you by <a href={"https://github.com/tinbob655"}>Tinbob655</a>
+            </p>
+            <img src={logo} alt={"The NewRinaldi logo"}/>
         </footer>
     )
 }
