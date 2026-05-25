0b06882eff29eb107762a15a03707110af97d184
Will Rutland <wrutland07@gmail.com>
2026-05-25 09:03:12 +0100

    started albums page

--- src/components/pages/albums/Albums.tsx
+++ src/components/pages/albums/Albums.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import PageHeader from "../../multiPage/PageHeader.tsx";
+import rawAlbumData from '../../../assets/albumsData.json' with { type: 'json' };
+import Player from "../../multiPage/player/Player.tsx";
 
 export default function Albums():React.ReactElement {
-
     return (
         <React.Fragment>
+            <PageHeader title={"Albums"} subtitle={"All my albums to date"} />
+            {getAlbumsHTML()}
         </React.Fragment>
     )
+}
+
+function getAlbumsHTML():React.ReactElement[] {
+    const res:React.ReactElement[] = [];
+    let index:number = 0;
+    rawAlbumData.forEach((album) => {
+        const alignment:string = index % 2 === 0 ? "alignRight" : "alignLeft";
+        res.push(
+            <React.Fragment>
+                <h2 className={alignment}>
+                    {album.frontendName}
+                </h2>
+                <p className={alignment}>
+                    Released: {album.releaseDate}
+                </p>
+
+                {getAlbumTracksHTML(album.tracks)}
+                <div className={"sectionDivider glow"} />
+            </React.Fragment>
+        )
+
+        index++;
+    })
+
+    return res;
+}
+
+function getAlbumTracksHTML(tracks: string[][]):React.ReactElement[] {
+    const res:React.ReactElement[] = [];
+    tracks.forEach((track) => {
+
+        //extract data
+        const fileName:string = track[0];
+        const trackName:string = track[1];
+
+        //create HTML
+        res.push(
+            <React.Fragment>
+                <p>
+                    {trackName}
+                </p>
+                <Player file={fileName} />
+                <div className={"sectionDivider"} />
+            </React.Fragment>
+        );
+    });
+
+    return res;
 }
